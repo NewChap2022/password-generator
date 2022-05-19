380ce7ddc63eeb91b5d291b69bc260d903945974
NewChap2022 <irisyangshu@gmail.com>
2022-05-18 22:09:10 -0400

    changing length coding

--- Develop/script.js
+++ Develop/script.js
@@ -2,24 +2,38 @@ const lowerCase = "abcdefghijklmnopqrstuvwyz";
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numeric = "0123456789";
 const specialCharacter = " !\"\#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+const lengthPrompt = function () {
+  var length = prompt("What is the the length of characters you want to set for your password?");
+  return length;
+}
 
 // Assignment code here
 function generatePassword() {
   window.alert("Welcome to use Password Generator! Please decide your requirements for your password.");
 
-  var minLength = ""
-  // ask for minimum length for password
-  while (minLength === "" || isNaN(minLength)) {
-    minLength = parseInt(window.prompt("What is the minimum length of characters you want to set for your password? \nPlease make sure you enter the numerals."));
-  }
-  console.log("The minimum length of password is " + minLength);
+  // ask for the length for password
+  var length = parseInt(lengthPrompt());
 
-  // ask for maximum length for password
-  var maxLength = ""
-  while (maxLength === "" || isNaN(maxLength) || maxLength < minLength) {
-    maxLength = parseInt(window.prompt("What is the maximum length of characters you want to set for your password? \nPlease make sure you enter the numerals and the maximum length is larger or equal to minimum length."));
+  while (length === "" || isNaN(length) || length < 8 || length > 128) {
+    switch (true) {
+      case (length === "" || isNaN(length)):
+        alert("Please enter the numerals");
+        length = parseInt(lengthPrompt());
+        break;
+      case (length < 8):
+        alert("The password should at least has 8 characters!")
+        length = parseInt(lengthPrompt());
+        break;
+
+      case (length > 128):
+        alert("The password should be no more than 128 characters!")
+        length = parseInt(lengthPrompt());
+        break;
+
+      default:
+        break;
+    }
   }
-  console.log("The maximum length of password is " + maxLength);
 
   var characters = "";
 
@@ -27,22 +41,22 @@ function generatePassword() {
     var characterSelect = window.confirm("Do you want to use lowercase for your password?")
     if (characterSelect) {
       characters = lowerCase;
-    } 
+    }
 
     characterSelect = window.confirm("Do you want to use uppercase for your password?")
     if (characterSelect) {
       characters += upperCase;
-    } 
+    }
 
     characterSelect = window.confirm("Do you want to use numeric for your password?")
     if (characterSelect) {
       characters += numeric;
-    } 
+    }
 
     characterSelect = window.confirm("Do you want to use special characters for your password?")
     if (characterSelect) {
       characters += specialCharacter;
-    } 
+    }
 
     if (characters === "" || characters === null) {
       window.alert("You need to choose at lease one type of characters to generate password!")
@@ -50,8 +64,6 @@ function generatePassword() {
   }
 
   // reference from stackoverFlow https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-  var length = Math.floor(Math.random() * (maxLength - minLength + 1) + minLength);
-  console.log(length);
   var result = "";
   var charactersLength = characters.length;
   for (var i = 0; i < length; i++) {
